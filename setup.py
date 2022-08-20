cdc6e5d5c1b3fa70fd8b86ee069ca798efa9145a
afraniomelo <afraeq@gmail.com>
2022-08-19 21:14:00 -0300

    Update requirements

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     name='kydlib',
     description='Routines for exploratory data analysis.',
     license="MIT",
-    version='0.1.1',
+    version='0.1.2',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/afraniomelo/kydlib',
